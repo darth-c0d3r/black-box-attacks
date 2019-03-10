77b31804726b39a6b7751f15231b09e18f828684
tnikhil <nikhiltummidi2002@gmail.com>
2019-03-10 15:19:31 +0530

    Papernot algo

--- white_box.py
+++ white_box.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.optim as optim
 import utils
+import numpy as np
 
 def adv_sample(model_name, dataset, target):
 	'''
@@ -35,7 +36,8 @@ def adv_sample(model_name, dataset, target):
 	for data, label in data_loader:
 
 		data = data.to(device)
-		sample, target = Variable(torch.randn(([1]+model.input_size)).to(device), requires_grad=True), Variable(target).to(device)
+		sample, target = Variable(torch.randn([1,1,28,28]).to(device), requires_grad=True), Variable(target).to(device)
+		print(sample.shape)
 
 		# sample = (sample - torch.min(sample)) / (torch.max(sample) - torch.min(sample))
 		sample = torch.clamp(sample,0,1)
@@ -63,5 +65,77 @@ def adv_sample(model_name, dataset, target):
 		inp = int(input("idx: "))
 		utils.disp_img(samples[inp], (100,100))
 		utils.disp_img(dataset[inp][0], (100,100))
+
+	return samples
+
+def adv_sample_papernot(model_name, dataset, target):
+	'''
+	data contains (image, target_original_label) from a dataset class
+	target is the class to which the data is being misclassified
+	'''
+	device = utils.get_device(1)
+
+	NUM_SAMPLES = 10
+
+	samples = []
+
+	L2_loss = nn.MSELoss().to(device)
+	Classification_loss = nn.CrossEntropyLoss().to(device)
+	
+	model = torch.load("saved_models/"+model_name).to(device)
+	data_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+
+	idx = 0
+	samples = torch.zeros([NUM_SAMPLES]+list(dataset[0][0].shape))
+
+	# init target
+	target_ = target
+	target = torch.zeros((1,model.n_classes))
+	target[0][target_] = 1
+
+	for data, label in data_loader:
+
+		data = data.to(device)
+		sample, target = Variable(torch.randn([1,1,28,28]).to(device), requires_grad=True), Variable(target).to(device)
+
+		sample = torch.clamp(sample,0,1)
+
+		sample, target = Variable(sample.data, requires_grad=True).to(device), Variable(target).to(device)
+		output = model(sample)
+		model.zero_grad()
+		output[0][target_].backward(retain_graph=True)
+
+		jacobian_t = sample.grad.data
+		for i in range(model.n_classes):
+			if i == target_:
+				continue
+			output[0][i].backward(retain_graph=True)
+
+		jacobian_non_t = sample.grad.data - jacobian_t
+
+		saliency = np.zeros(sample.shape)
+		for i in range(28):
+			for j in range(28):
+				if jacobian_t[0][0][i][j]<0 or jacobian_non_t[0][0][i][j]>0:
+					continue
+				saliency[i] = -jacobian_t[0][0][i][j]*jacobian_non_t[0][0][i][j]
+
+		indices = np.argsort(saliency)
+
+		# for i in range(size(indices)):
+		# sample = sample + saliency
+
+		sample = torch.clamp(sample,0,1)
+
+		samples[idx] = sample[0]
+		idx += 1
+
+		if idx == NUM_SAMPLES:
+			break
+
+	# for i in range(5):
+	# 	inp = int(input("idx: "))
+	# 	utils.disp_img(samples[inp], (100,100))
+	# 	utils.disp_img(dataset[inp][0], (100,100))
 
 	return samples
